06006dea540b721a1dc2e3feca2d970b42e24104
narmadaharikrishnan <162294888+narmadaharikrishnan@users.noreply.github.com>
2024-03-12 17:57:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask , render_template, Request,session
 app = Flask(__name__)
 @app.route('/')
 def login():
-    return 'login.html'
+    return render_template ('login.html') 
 if __name__=="__main__":
     app.run(debug=True)         
