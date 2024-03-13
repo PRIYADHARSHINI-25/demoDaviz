723a4ba1a120b193c7a10164b3a9d9ada4c9764b
skycake10 <nandhinipalani1010@gmail.com>
2024-03-13 09:56:53 +0530

    added sign , login

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ app = Flask(__name__)
 @app.route('/')
 def login():
     return render_template ('login.html') 
+@app.route("/sign up")
+def sign up():
 if __name__=="__main__":
     app.run(debug=True)         
