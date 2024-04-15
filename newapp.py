6dc76bbd64457cb8fce7ab6c1695bfd6be326bc5
PRIYADHARSHINI-25 <priya35471@gmail.com>
2024-04-15 22:54:15 +0530

    keyupdation

--- newapp.py
+++ newapp.py
@@ -1,11 +1,14 @@
 from flask import Flask, url_for, session, render_template, redirect, abort
 from authlib.integrations.flask_client import OAuth
-
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+def config():
+    load_dotenv()
 appConfiguration = {
-    "DAVIZ_CLIENT_ID": "45114726904-qk6osjb235cu288euphestgd36pbvcp0.apps.googleusercontent.com",
-    "DAVIZ_CLIENT_SECRET": "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",
+    # "DAVIZ_CLIENT_ID": "45114726904-1v6o2v0etu1l60fhu90msogrjehksmgg.apps.googleusercontent.com",
+    # "DAVIZ_CLIENT_SECRET": "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",
     "FLASK_SECRET": "b7ad6c36-dbac-43cc-a540-c87630c77181",
     "FLASK_PORT": 5000
 }
@@ -16,8 +19,8 @@ CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
 oauth = OAuth(app)
 oauth.register(
     name='daviz',
-    client_id=appConfiguration.get("DAVIZ_CLIENT_ID"),
-    client_secret=appConfiguration.get("DAVIZ_CLIENT_SECRET"),
+    client_id=os.getenv('clientID'),
+    client_secret=os.getenv('clientsecret'),
     server_metadata_url=CONF_URL,
     client_kwargs={
         'scope': 'openid email profile'
@@ -43,11 +46,14 @@ def gsignin():
     session['user'] = token['userinfo']
     return redirect('/')
 
-
 @app.route('/logout')
 def logout():
     session.pop('user', None)
     return redirect('/')
+
+@app.route("/",methods=['GET','POST'])
+def fileUpload():
+    return render_template("FileUpload.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=appConfiguration.get("FLASK_PORT"), debug=True)
