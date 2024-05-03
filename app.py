8cc6dd6b968036d03340450987fc3031a64ee3e2
PRIYADHARSHINI-25 <priya35471@gmail.com>
2024-05-03 18:06:28 +0530

    ok

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from authlib.integrations.flask_client import OAuth
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 import pandas as pd
-import os
+import os, jsonify
 import gridfs
-from charts import preproces
+from charts import preprocess,chartvis
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Daviz"
@@ -30,22 +30,14 @@ oauth.register(
 
 @app.route('/')
 def home():
-        user = session.get('user')
-        name=user['name']
-        email=user['email']
-        profile=user['picture']
-        # verify=user['email_verified']
-        query={'email_id':email}
-        doc ={'$set':{'email_id':email,'name':name,'profile':profile}}
-        mongo.db.user.update_one(query,doc,upsert=True)
-        return render_template('login.html', user=name)
+        return render_template('home.html')
 
 
 
 @app.route('/login')
 def login():
     if "user" in session:
-        abort(404)    
+        return render_template('home.html',user=session.get('user'))   
     return oauth.daviz.authorize_redirect(redirect_uri=url_for('gsignin', _external=True))
 
 
@@ -53,14 +45,23 @@ def login():
 def gsignin():
     token = oauth.daviz.authorize_access_token()
     session['user'] = token['userinfo']
-    return redirect('/')
+    user = session.get('user')
+    name=user['name']
+    email=user['email']
+    profile=user['picture']
+        # verify=user['email_verified']
+    query={'email_id':email}
+    doc ={'$set':{'email_id':email,'name':name,'profile':profile}}
+    mongo.db.user.update_one(query,doc,upsert=True)
+    return render_template('login.html', user=name)
+    # return redirect('/')
 
 @app.route('/logout')
 def logout():
     session.pop('user', None)
     return render_template("home.html")
 
-app.config["UPLOAD_FOLDER1"]="static/csvfiles"
+# app.config["UPLOAD_FOLDER1"]="static/csvfiles"
 
 # @app.route("/",methods=['GET','POST'])
 # def upload():
@@ -75,21 +76,46 @@ app.config["UPLOAD_FOLDER1"]="static/csvfiles"
 #     return render_template("home.html")
 
 filegrid=gridfs.GridFS(mongo.db)
-@app.route("/",methods=['GET','POST'])
-def upload():
+@app.route("/chart",methods=['GET','POST'])
+def chart():
     user = session.get('user')
     email=user['email']
     if request.method=='POST':
         upload_csv=request.files['upload_file']
+        # xvar=request.form.get('xAxis')
+        # yvar=request.form.get('yAxis')
+        # charttype=request.form.get('charttype')
+        # print(xvar,yvar,charttype)
+        # content.seek(0)
         content=upload_csv.read()
         csv_id=filegrid.put(content,filename=upload_csv.filename)
-        # upload_csv.seek(0)
         mongo.db.user.update_one({'email_id':email},{'$set':{'files':csv_id}})
         grid_out = filegrid.get(csv_id)
         data = grid_out.read()
-        chart=preproces(data)
-        return render_template("chart.html",data=chart)
+        option,df=preprocess(data)
+        session['df'] = df.to_json()
+        types=['line','bar','pie']
+        # if xvar and yvar and charttype:
+        #     chart_user= chartvis(df,xvar,yvar,charttype)
+        #     return render_template("chart.html",data=chart_user)
+        return render_template("chart.html",option=option,types=types)
     return render_template("home.html")
 
+@app.route("/visualize",methods=['GET','POST'])
+def visualize():
+    if 'df' in session:
+        df = pd.read_json(session['df']) 
+    if request.method=='POST':
+         # Get the AJAX data sent from the HTML
+        charttype=request.form.get('charttyype')
+        xvar=request.form.get('xvar')
+        yvar=request.form.get('yvar')
+        print(xvar, yvar, charttype)
+        if xvar and yvar and charttype:
+            chart_user= chartvis(df,xvar,yvar,charttype)
+            return render_template("chart.html",data=chart_user)
+        else:
+            return "Give valid input"
+
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=os.getenv('flask_port'), debug=True)
+    app.run(port=os.getenv('flask_port'), debug=True)
--- charts.py
+++ charts.py
@@ -4,25 +4,28 @@ from highcharts_core.options import HighchartsOptions
 # from highcharts_core.options.series.line import LineSeries
 from highcharts_core.options.series.bar import BarSeries
 import json
-
+import numpy as np
 import pandas as pd
 from io import StringIO
 
-def preproces(data):
+def preprocess(data):
     data_str=data.decode('utf-8-sig')
     data_io=StringIO(data_str)
-
     df=pd.read_csv(data_io)
-    charts=chart(df)
-    return(charts)
+    options=list(df.columns)
+    # print(df.head())
+    # charts=chart(df)
+    return(options,df)
 
-def chart(df):
+def chartvis(df,xvar,yvar,charttype):
     mychart=Chart.from_pandas(df,
-                              property_map={'x':'Name',
-                                            'y':'Class'},
-                              series_type='bar',
+                              property_map={'x':xvar,
+                                            'y':yvar},
+                                            # 'y':['Age 16-19','Age 20-24','Age 25-34','Age 35-44']},
+                              series_type=charttype,
                               chart_kwargs = {'container': 'target_div',
                                               'variable_name': 'myChart'
   })
     chart=mychart.to_js_literal()
+    # print(chart)
     return(chart)
